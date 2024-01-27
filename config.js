494347a0f71c3605f800872281574f5d29b91ba4
secredasho <146038531+secredasho@users.noreply.github.com>
2024-01-27 11:22:07 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    david: 'developer',
   },
 }
 export default config
